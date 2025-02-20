85e7a713e291c8252b750db080980f8bbe8ac0ef
DanielCDoran <40theridgeway@gmail.com>
2025-02-20 12:37:00 +0000

    basic cotroller and service tests used. Need to be expanded

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,11 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService} from  './users.service'
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
+
+  const mockUsersService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },],
       controllers: [UsersController],
     }).compile();
 
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,13 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
-
+import { User } from './user.entity';
+import { UserDto } from './user.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
+  const mockUserRepository = {
+    save: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -16,4 +30,29 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // it('create => Should create a new user and return its data', async () => {
+  //   // arrange
+  //   const userDto = {
+  //     name: 'Chadwick',
+  //     email: 'chadwickboseman@email.com',
+  //   } as UserDto;
+
+  //   const user = {
+  //     id: 1,
+  //     name: 'Chadwick',
+  //     email: 'chadwickboseman@email.com',
+  //   } as User;
+
+  //   jest.spyOn(mockUserRepository, 'save').mockReturnValue(user);
+
+  //   // act
+  //   const result = await service.create(user);
+
+  //   // assert
+  //   expect(mockUserRepository.save).toBeCalled();
+  //   expect(mockUserRepository.save).toBeCalledWith(createUserDto);
+
+  //   expect(result).toEqual(user);
+  // });
 });
