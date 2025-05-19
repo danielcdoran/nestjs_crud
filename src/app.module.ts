51ae460aa874966b131934d449cc8fc303fff36b
DanielCDoran <40theridgeway@gmail.com>
2025-05-19 13:06:43 +0100

    docker compose up works but devcontainer doesnt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-// import { LoggerModule } from 'nestjs-pino';
-
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as string,
-      host: process.env.PG_HOST,
-      port: parseInt(process.env.PG_PORT || ''),
-      username: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DB,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PG_HOST'),
+        port: +configService.get('PG_PORT'),
+        username: configService.get('PG_USER'),
+        password: configService.get('PG_PASSWORD'),
+        database: configService.get('PG_DB'),
+        // entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        // do NOT use synchronize: true in real projects
+        synchronize: true,
+      }),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
